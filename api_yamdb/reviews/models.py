5098c0bc2bd095ca07311900122b6b598c303131
Catherine Rain <catherinerain@MacBook-Air-Catherine.local>
2022-01-19 22:09:59 +0300

    flake8

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,7 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator
 from users.models import User
 
-from django.core.validators import MaxValueValidator
 from .validators import validator_year
 
 
