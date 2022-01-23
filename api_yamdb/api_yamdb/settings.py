e472f390244ca38f13353796b5fe7ca40cefb441
Catherine Rain <catherinerain@MacBook-Air-Catherine.local>
2022-01-23 14:19:19 +0300

    rEfactor dockerfile

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,12 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '51.250.30.224',
-    '0.0.0.0',
-    '127.0.0.1',
-    'localhost'
-]
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
